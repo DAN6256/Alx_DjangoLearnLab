9d0da3aed166e4840203ecf53096fc40a2efb693
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-09-06 10:56:40 +0000

    correcct register

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import CustomLoginView, CustomLogoutView, register, list_books, LibraryDetailView
+from .views import CustomLoginView, CustomLogoutView, register, list_books, LibraryDetailView, views
 
 urlpatterns = [
     path('books/', list_books, name='book-list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
     path('login/', CustomLoginView.as_view(template_name="relationship_app/login.html"), name='login'),
     path('logout/', CustomLogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
 ]
