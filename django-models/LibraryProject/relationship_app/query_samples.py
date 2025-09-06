a2e1e4f06057de57e2052ff6481a1e118065136b
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-09-06 10:06:10 +0000

    Add query for librarian

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -14,4 +14,4 @@ library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 
 # Retrieve the librarian for that library
-library_name# Or: librarian = Librarian.objects.get(library=library)
+librarian = Librarian.objects.get(library=library)
