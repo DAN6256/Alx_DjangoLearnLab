3a8d8984c108589c194ccd3e087ff350bf0b4d13
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-09-06 10:21:13 +0000

    Refine relations

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,27 +1,23 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
-from .models import Book,Library
-# Create your views here.
+from .models import Book, Library
 
-
+# Function-based view to list all books
 def list_books(request):
     """
-    Function-based view to list all books and their authors.
-    Returns a plain text response with each book on a new line.
+    Renders a list of all books and their authors.
     """
     books = Book.objects.select_related('author').all()
-    output_lines = [f"{book.title} by {book.author.name}" for book in books]
-    response_text = "\n".join(output_lines)
-    return HttpResponse(response_text, content_type="text/plain")
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-
+# Class-based view to show details of a specific library
 class LibraryDetailView(DetailView):
     """
     Displays details for a specific Library, including all associated Books.
     """
     model = Library
-    template_name = 'relationship_app/library_detail.html'  
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
