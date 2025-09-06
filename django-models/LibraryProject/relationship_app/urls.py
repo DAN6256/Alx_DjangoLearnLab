d6b528ae55f4d10cfaaa7779fba4821af186dbbd
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-09-06 10:54:51 +0000

    Add relationship names

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,7 +4,7 @@ from .views import CustomLoginView, CustomLogoutView, register, list_books, Libr
 urlpatterns = [
     path('books/', list_books, name='book-list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('login/', CustomLoginView.as_view(template_name="relationship_app/login.html"), name='login'),
+    path('logout/', CustomLogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
     path('register/', register, name='register'),
 ]
