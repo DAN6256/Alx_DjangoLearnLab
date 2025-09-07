3bee204d9ed1c6deec288f61078406784761848c
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-09-07 14:59:51 +0000

    Update role based access

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,7 +8,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
 from django.views import View
-
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Library
@@ -38,26 +39,28 @@ class LibraryDetailView(DetailView):
     
 
 
-# def user_login(request):
-#     """
-#     Handles user login using Django's AuthenticationForm.
-#     """
-#     if request.method == 'POST':
-#         form = AuthenticationForm(request, data=request.POST)
-#         if form.is_valid():
-#             user = form.get_user()
-#             login(request, user)
-#             return redirect('book-list')  # Redirect to a meaningful page
-#     else:
-#         form = AuthenticationForm()
-#     return render(request, 'relationship_app/login.html', {'form': form})
 
-# def user_logout(request):
-#     """
-#     Logs out the current user and renders a confirmation page.
-#     """
-#     logout(request)
-#     return render(request, 'relationship_app/logout.html')
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 def register(request):
     """
