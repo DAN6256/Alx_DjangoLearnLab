4171c4ced62cdc6082af9de38b8ae4040cb4d048
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-09-07 14:55:44 +0000

    role based access

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views import CustomLoginView, CustomLogoutView, register, list_books, LibraryDetailView, views
-
+from .admin_view import admin_view
+from .librarian_view import librarian_view
+from .member_view import member_view
 urlpatterns = [
     path('books/', list_books, name='book-list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
     path('login/', CustomLoginView.as_view(template_name="relationship_app/login.html"), name='login'),
     path('logout/', CustomLogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
     path('register/', views.register, name='register'),
+    path('admin-area/', admin_view.admin_view, name='admin-view'),
+    path('librarian-area/', librarian_view.librarian_view, name='librarian-view'),
+    path('member-area/', member_view.member_view, name='member-view'),
 ]
